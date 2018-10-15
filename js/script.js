68a38f53ecf0dc353863a08f8e152df5c6653a0b
KaurDim <kaurdim@gmail.com>
2018-10-15 21:16:52 +0300

    Экзамен HTML

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+	var number = 5;
+	var activeLink = document.getElementsByName("sliderButton");
+
+	var chekButton = document.getElementById("loosser");
+
+	console.log(chekButton);
+
+	chekButton.onclick = function() {
+		chekButton.classList.add("active");
+	};
+
+	
+
+
+
